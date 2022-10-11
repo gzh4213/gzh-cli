3b098b164daf2c6e1defbac785172567c155c551
guozhen <guozhen4213@126.com>
2022-10-11 11:58:45 +0800

    feat: 测试create功能

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
-# gzh-cli
+# gzh CLI  
+脚手架工具开发学习
+## 支持功能
+
+* 快速创建项目模板
+
+## Install  
+
+```bash
+
+# 全局安装cli
+npm install gzh-cli -g
+
+# 本地node版本如安装在系统目录下，可能需要sudo执行
+sudo npm install gzh-cli -g
+
+# 如果安装有`node-sass`报错 请尝试:
+npm install gzh-cli -g --unsafe-perm  
+```
+
+## Usage  
+
+```bash
+gzh-cli -v # 查看版本
+gzh-cli -h # 查看帮助
+
+# 创建新项目
+gzh-cli create wxapp-test
+```
--- bin/index.js
+++ bin/index.js
@@ -1,60 +1,15 @@
 #! /usr/bin/env node
-const chalk = require('chalk')
-// 开始处理命令
-const program = require('commander')
-const minimist = require('minimist')
+const {program} = require('commander')
 
 program
-	.version(require('../package.json').version, '-V --version')
-	.usage('<command> [options]')
-
-// 创建命令
-program
-	.command('create <project-name>')
-	.description('create a new project')
-	.option(
-		'-p, --preset <presetName>',
-		'Skip prompts and use saved or remote preset'
-	)
-	.option('-d, --default', 'Skip prompts and use default preset')
-	.action((name, cmd) => {
-		const options = cleanArgs(cmd)
-		if (minimist(process.argv.slice(3))._.length > 1) {
-			console.log(
-				chalk.yellow(
-					'\n ⚠️  检测到您输入了多个名称，将以第一个参数为项目名，舍弃后续参数哦'
-				)
-			)
-		}
-		require('../lib/create')(name, options)
-	})
-
-program.arguments('<command>').action((cmd) => {
-	program.outputHelp()
-	console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
-	console.log()
-	// suggestCommands(cmd)
-})
-
-// 调用
-program.parse(process.argv)
-
-if (!process.argv.slice(2).length) {
-	program.outputHelp()
-}
-
-function camelize(str) {
-	return str.replace(/-(\w)/g, (_, c) => (c ? c.toUpperCase() : ''))
-}
-// 获取参数
-function cleanArgs(cmd) {
-	const args = {}
-	cmd.options.forEach((o) => {
-		const key = camelize(o.long.replace(/^--/, ''))
-		// 如果没有传递option或者有与之相同的命令，则不被拷贝
-		if (typeof cmd[key] !== 'function' && typeof cmd[key] !== 'undefined') {
-			args[key] = cmd[key]
-		}
-	})
-	return args
-}
+  .name('gzh-cli')
+  .usage(`<command> [option]`)
+  .version(`gzh-cli ${require('../package.json').version}`)
+  .command('create <project-name>')
+  .description('create a new miniprogram template project')
+  .option("-f, --force", "overwrite target directory if it exists")
+  .action((projectName, cmd) => {
+    // 调用create模块
+    require('../lib/cmd/create')(projectName, cmd)
+  });
+program.parse()
